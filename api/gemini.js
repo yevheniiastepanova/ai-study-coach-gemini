d76ff5720ee35e44eee3b472ce11bd3f8f17a97e
Yevheniia <evgeniyalexandrovna94@gmail.com>
2025-12-22 22:10:56 -0500

    Use gemini-1.5-flash model for backend

--- api/gemini.js
+++ api/gemini.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
     const model = genAI.getGenerativeModel({
-     model: "gemini-1.0-pro", 
+    model: "gemini-1.5-flash",
     });
 
     const result = await model.generateContent(prompt);
